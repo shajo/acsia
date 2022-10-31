4c750c537b607cf02388113f232ae91b893ca37f
shajo <shajo@ndimensionz.com>
2022-10-31 19:15:11 +0530

    files are adding to the repo

---
+++ js/demo/chart-pie-demo.js
@@ -0,0 +1,35 @@
+// Set new default font family and font color to mimic Bootstrap's default styling
+Chart.defaults.global.defaultFontFamily = 'Montserrat';
+Chart.defaults.global.defaultFontColor = '#858796';
+
+// Pie Chart Example
+var ctx = document.getElementById("myPieChart");
+var myPieChart = new Chart(ctx, {
+  type: 'doughnut',
+  data: {
+    labels: ["Active", "Inactive",],
+    datasets: [{
+      data: [60, 40],
+      backgroundColor: ['#435EBE', '#56C8E9'],
+      hoverBackgroundColor: ['#274193', '#2e7f96'],
+      hoverBorderColor: "rgba(234, 236, 244, 1)",
+    }],
+  },
+  options: {
+    maintainAspectRatio: false,
+    tooltips: {
+      backgroundColor: "rgb(255,255,255)",
+      bodyFontColor: "#858796",
+      borderColor: '#dddfeb',
+      borderWidth: 1,
+      xPadding: 15,
+      yPadding: 15,
+      displayColors: false,
+      caretPadding: 10,
+    },
+    legend: {
+      display: false
+    },
+    cutoutPercentage: 80,
+  },
+});
